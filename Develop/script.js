db58561cf254160186d8b3448c309d0323f9a69c
James Holding <james.holding90@gmail.com>
2020-06-14 19:05:56 -0400

    added in timeSlot change of class based on currentHour.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,4 @@
-// GET CURRENT TIME AND DATE 
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm:ss a'));
-
-// CURRENT HOUR - used to update time slots 
-var currentHour = parseInt(moment().format("HH"));
-console.log(currentHour);
-
-
-// ESTABLISH VARIABLES FROM CURRENT HTML 
+// ESTABLISH VARIABLES FROM CURRENT HTML
 var nine = $("#9");
 var ten = $("#10");
 var eleven = $("#11");
@@ -17,4 +9,27 @@ var three = $("#15");
 var four = $("#16");
 var five = $("#17");
 
-// ADD FUNCTION TO RECOGNIZE WHAT TIME IT IS AND UPDATE THE TIMESLOTS.
+
+// GET CURRENT TIME AND DATE
+$("#currentDay").text(
+  moment().format("dddd MMMM Do YYYY, h:mm:ss a")
+);
+
+// CURRENT HOUR - used to update time slots
+var currentHour = parseInt(moment().format("HH"));
+console.log(currentHour);
+
+// FUNCTION TO RECOGNIZE WHAT TIME IT IS AND UPDATE THE TIMESLOTS.
+$("textarea").each(function () {
+  var timeSlot = parseInt($(this).attr("id"));
+  if (timeSlot < currentHour) {
+    $(this).addClass("past");
+  } else if (timeSlot === hour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+  } else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  }
+});
