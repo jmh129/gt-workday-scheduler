a0ecb8de30140493751d48ffd460fd353d4989a8
James Holding <james.holding90@gmail.com>
2020-06-14 17:39:04 -0400

    variables for each time slot created.

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,13 @@
 // GET CURRENT TIME AND DATE 
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+// ESTABLISH VARIABLES FROM CURRENT HTML 
+var nine = $("#9");
+var ten = $("#10");
+var eleven = $("#11");
+var twelve = $("#12");
+var one = $("#13");
+var two = $("#14");
+var three = $("#15");
+var four = $("#16");
+var five = $("#17");
